ae8d1eaa92af4ab3f1a8d78aec68f374f4cf03a2
Siddhesh Kachare <siddheshkachare111@gmail.com>
2024-08-07 13:42:29 +0530

    'Setup'

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hyphen_edot = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+        if hyphen_edot in requirements:
+            requirements.remove(hyphen_edot)
+        
+    return requirements
+
+
+setup(
+name = 'MLProject',
+version = '0.0.1',
+author = 'Siddhesh Kachare',
+author_email = 'siddheshkachare111@gmail.com',
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+
+)
